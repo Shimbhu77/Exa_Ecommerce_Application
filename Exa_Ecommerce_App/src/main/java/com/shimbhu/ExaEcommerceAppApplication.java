0b397640c374a8cdd51d568663308c1c27e9c3b1
Shimbhu77 <kumawatshimbhu4@gmail.com>
2022-11-18 17:28:26 +0530

    day1

---
+++ Exa_Ecommerce_App/src/main/java/com/shimbhu/ExaEcommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.shimbhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaEcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaEcommerceAppApplication.class, args);
+	}
+
+}
